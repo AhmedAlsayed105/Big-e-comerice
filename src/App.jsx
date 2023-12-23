0873e3aaa7516b0381aa570edb45901dd38bdffe
AhmedAlsayed105 <95216883+AhmedAlsayed105@users.noreply.github.com>
2023-12-23 20:54:22 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Button from '@mui/material/Button';
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline block text-yellow-500 ">
+        Hello world!
+      </h1>
+      <Button variant="contained">Hello world</Button>;
+    </>
+  )
+}
+
+export default App
